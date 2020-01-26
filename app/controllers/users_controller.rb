2e11a6890ddee8b05ff0c3dfad2e9f1f51484d3a
nlisauskas <nlisausk@alumni.nd.edu>
2020-01-26 13:43:57 -0600

    changed user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-        binding.pry
         UserMailer.with(user: @user).welcome_email.deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
