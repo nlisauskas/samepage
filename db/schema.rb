c72ec9259b44e046390b1af5832db4ed7dddc7ea
nlisauskas <nlisausk@alumni.nd.edu>
2020-01-26 13:09:28 -0600

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_20_191453) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "maintenance_requests", force: :cascade do |t|
     t.string "category"
     t.string "description"
