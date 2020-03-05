da21ab1ac17063bcf0a460b18a26b39ef1e43538
nlisauskas <nlisausk@alumni.nd.edu>
2020-03-05 07:38:37 -0600

    adjusting pw reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,7 +3,6 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    binding.pry
     if params[:category] == 'Landlord'
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
@@ -26,7 +25,6 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    binding.pry
     if User.find_by_password_reset_token(params[:id])
     @user = User.find_by_password_reset_token(params[:id])
     else
